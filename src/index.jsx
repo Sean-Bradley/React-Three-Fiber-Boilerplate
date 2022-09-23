2d2dc6821517f205fdd105218aa14d6673fb9de4
Sean Bradley <seanwasere@gmail.com>
2022-09-23 22:14:30 +0100

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import './styles.css'
 
-function App() {
-  return (
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Canvas camera={{ position: [0, 0, 2] }}>
       <mesh>
         <boxGeometry />
         <meshBasicMaterial color={0x00ff00} wireframe={true} />
       </mesh>
     </Canvas>
-  )
-}
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
   </StrictMode>
 )
