82f6148745a2f60a4c8995f4b054d223597f72c8
Sean Bradley <seanwasere@gmail.com>
2022-09-26 09:05:59 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Box from './Box'
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box position={[-0.75, 0, 0]} name="A"/>
-      <Box position={[0.75, 0, 0]} name="B"/>
+      <Box position={[-0.75, 0, 0]} name="A" />
+      <Box position={[0.75, 0, 0]} name="B" />
     </Canvas>
   )
 }
