5e2ac088cc3a7837a969b6e550c63c8afedf9aba
Sean Bradley <seanwasere@gmail.com>
2022-09-28 18:29:40 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { Canvas, extend } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
-import {
-  AxesHelper,
-  BoxGeometry,
-  SphereGeometry,
-  DodecahedronGeometry
-} from 'three'
+import * as THREE from 'three'
 import { useMemo } from 'react'
 import { Stats, OrbitControls } from '@react-three/drei'
-
-extend({ AxesHelper })
 
 export default function App() {
   const polyhedron = useMemo(
     () => [
-      new BoxGeometry(),
-      new SphereGeometry(0.785398),
-      new DodecahedronGeometry(0.785398)
+      new THREE.BoxGeometry(),
+      new THREE.SphereGeometry(0.785398),
+      new THREE.DodecahedronGeometry(0.785398)
     ],
     []
   )
@@ -40,7 +33,7 @@ export default function App() {
         userData-polyhedron={polyhedron}
       />
       <OrbitControls />
-      <axesHelper args={[5]}/>
+      <axesHelper args={[5]} />
       <Stats />
     </Canvas>
   )
