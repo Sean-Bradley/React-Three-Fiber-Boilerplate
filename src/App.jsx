3357e6a705a517acefda2c71aac0ea1e8dde6670
Sean Bradley <seanwasere@gmail.com>
2022-10-07 15:20:34 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <>
       <Canvas camera={{ position: [-1, 4, 2.5] }}>
-        <directionalLight position={[1, 1, 1]}></directionalLight>
+        <directionalLight position={[1, 1, 1]} />
         <Polyhedron name="meshBasicMaterial" position={[-3, 1, 0]} material={new THREE.MeshBasicMaterial()} />
         <Polyhedron name="meshNormalMaterial" position={[-1, 1, 0]} material={new THREE.MeshNormalMaterial()} />
         <Polyhedron name="meshPhongMaterial" position={[1, 1, 0]} material={new THREE.MeshPhongMaterial()} />
