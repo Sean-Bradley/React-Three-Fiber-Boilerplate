23f9b9acb655c7c54fdf06c8eebe662468cbee2f
Sean Bradley <seanwasere@gmail.com>
2022-10-08 16:44:06 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Stats, OrbitControls, Circle } from '@react-three/drei'
-import { useRef, useEffect } from 'react'
+import { useRef } from 'react'
 import { MeshStandardMaterial } from 'three'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
@@ -8,18 +8,10 @@ export default function App() {
   const directionalRef = useRef()
   const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
-  useEffect(() => {
-    gltf.scene.children.forEach((c) => {
-      if (c.isMesh) {
-        c.castShadow = true
-      }
-    })
-  })
-
   return (
     <>
       <directionalLight ref={directionalRef} position={[3.3, 1.0, 4.4]} castShadow={true} />
-      <primitive object={gltf.scene} position={[0, 1, 0]} onChange={console.log(gltf.scene)} />
+      <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
       <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true} material={new MeshStandardMaterial()} />
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
