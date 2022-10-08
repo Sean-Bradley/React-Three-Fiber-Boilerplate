2a2a699f882740eab26c896345d80c9d9138d7a5
Sean Bradley <seanwasere@gmail.com>
2022-10-08 16:14:16 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Stats, OrbitControls } from '@react-three/drei'
+import { Stats, OrbitControls, Circle } from '@react-three/drei'
 import { useRef, useEffect } from 'react'
-import Floor from './Floor'
+import { MeshStandardMaterial } from 'three'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -20,7 +20,7 @@ export default function App() {
     <>
       <directionalLight ref={directionalRef} position={[3.3, 1.0, 4.4]} castShadow={true} />
       <primitive object={gltf.scene} position={[0, 1, 0]} onChange={console.log(gltf.scene)} />
-      <Floor />
+      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true} material={new MeshStandardMaterial()} />
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
--- src/Floor.jsx
+++
@@ -1,8 +0,0 @@
-export default function Floor() {
-    return (
-      <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
-        <circleGeometry args={[10]} />
-        <meshStandardMaterial />
-      </mesh>
-    )
-  }
