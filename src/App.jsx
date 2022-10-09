35ae491607a2a0782f60c61003ad72b41af4e87a
Sean Bradley <seanwasere@gmail.com>
2022-10-09 22:37:38 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Stats, OrbitControls, Environment } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -7,12 +8,14 @@ export default function App() {
 
   return (
     <>
-      <Environment files="./img/venice_sunset_1k.hdr" background />
-      <directionalLight position={[3.3, 1.0, 4.4]} />
-      <primitive object={gltf.scene} position={[0, 1, 0]} />
-      <OrbitControls target={[0, 1, 0]} autoRotate />
-      <axesHelper args={[5]} />
-      <Stats />
+      <Canvas camera={{ position: [-0.5, 1, 2] }}>
+        <Environment files="./img/venice_sunset_1k.hdr" background />
+        <directionalLight position={[3.3, 1.0, 4.4]} />
+        <primitive object={gltf.scene} position={[0, 1, 0]} />
+        <OrbitControls target={[0, 1, 0]} autoRotate />
+        <axesHelper args={[5]} />
+        <Stats />
+      </Canvas>
     </>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
 import './styles.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Canvas camera={{ position: [-0.5, 1, 2] }}>
-      <App />
-    </Canvas>
+    <App />
   </StrictMode>
 )
