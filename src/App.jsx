8ee41bece6ee3f1d013a03f385a41a78473e1c1c
Sean Bradley <seanwasere@gmail.com>
2022-10-09 22:33:27 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import { useRef } from 'react'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
@@ -9,14 +10,16 @@ export default function App() {
 
   return (
     <>
-      <directionalLight ref={directionalRef} position={[3.3, 1.0, 4.4]} castShadow={true} />
-      <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true}>
-        <meshStandardMaterial />
-      </Circle>
-      <OrbitControls target={[0, 1, 0]} />
-      <axesHelper args={[5]} />
-      <Stats />
+      <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+        <directionalLight ref={directionalRef} position={[3.3, 1.0, 4.4]} castShadow={true} />
+        <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
+        <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true}>
+          <meshStandardMaterial />
+        </Circle>
+        <OrbitControls target={[0, 1, 0]} />
+        <axesHelper args={[5]} />
+        <Stats />
+      </Canvas>
     </>
   )
 }
