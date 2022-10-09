56236677bcc3d46ae2d52b3265682f1c8123089d
Sean Bradley <seanwasere@gmail.com>
2022-10-09 11:26:44 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Stats, OrbitControls, Environment } from '@react-three/drei'
-import { useRef } from 'react'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function App() {
-  const directionalRef = useRef()
   const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
   return (
     <>
       <Environment files="./img/venice_sunset_1k.hdr" background />
-      <directionalLight ref={directionalRef} position={[3.3, 1.0, 4.4]} castShadow={true} />
+      <directionalLight position={[3.3, 1.0, 4.4]} castShadow={true} />
       <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
       <OrbitControls target={[0, 1, 0]} autoRotate />
       <axesHelper args={[5]} />
