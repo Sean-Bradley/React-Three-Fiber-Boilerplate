b4562288b891845f01bec55f82fd30c4cbf4cf52
Sean Bradley <seanwasere@gmail.com>
2022-10-09 13:55:26 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <>
       <Environment files="./img/venice_sunset_1k.hdr" background />
-      <directionalLight position={[3.3, 1.0, 4.4]} castShadow={true} />
-      <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
+      <directionalLight position={[3.3, 1.0, 4.4]} />
+      <primitive object={gltf.scene} position={[0, 1, 0]} />
       <OrbitControls target={[0, 1, 0]} autoRotate />
       <axesHelper args={[5]} />
       <Stats />
