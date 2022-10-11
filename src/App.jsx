64258bd6af02ca51a1d431901909b2bb02ea4359
Sean Bradley <seanwasere@gmail.com>
2022-10-11 11:36:13 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls, Environment } from '@react-three/drei'
+import { useGLTF, OrbitControls, Environment, Html } from '@react-three/drei'
 import { Leva, useControls } from 'leva'
 
 const MODELS = {
@@ -10,7 +10,27 @@ const MODELS = {
 
 function Model(props) {
   const { scene } = useGLTF(props.url)
-  return <primitive object={scene} />
+  const annotations = []
+
+  scene.traverse((o) => {
+    if (o.userData.prop) {
+      annotations.push(
+        <Html key={o.uuid} position={[o.position.x, o.position.y, o.position.z]} distanceFactor={0.25}>
+          <div className="annotation">{o.userData.prop}</div>
+        </Html>
+      )
+    }
+  })
+
+  return (
+    <>
+      <primitive object={scene}>
+        {annotations.map((a) => {
+          return a
+        })}
+      </primitive>
+    </>
+  )
 }
 
 export default function App() {
@@ -23,7 +43,7 @@ export default function App() {
 
   return (
     <>
-      <Canvas camera={{ position: [0, 0, -0.2], near: 0.05 }}>
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
         <Environment files="./img/workshop_1k.hdr" background />
         <group>
           <Model url={MODELS[dropDown.model]} />
