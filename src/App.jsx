a2eea2ec5d1617c4b612f1a0c4c3f6204902c179
Sean Bradley <seanwasere@gmail.com>
2022-10-11 20:18:05 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,132 @@
 import { Stats, OrbitControls, useGLTF, Environment } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
-import { Leva, useControls } from 'leva'
-import { MeshBasicMaterial } from 'three'
+import { useRef, useState } from 'react'
+import { Leva, useControls, button } from 'leva'
+import { Vector3 } from 'three'
 
 function Lights() {
-  const ref = useRef()
-
-  useControls('Directional Light', {
-    position: {
-      x: 65.0,
-      y: 21.0,
-      z: 86.0,
-      onChange: (v) => {
-        ref.current.position.copy(v)
-        console.log(ref.current)
-      }
-    },
-    left: {
-      value: -30,
-      min: -30,
-      max: 30,
-      step: 0.1,
-      onChange: (v) => {
-        ref.current.shadow.camera.left = v
-        ref.current.shadow.camera.updateProjectionMatrix()
-      }
-    },
-    right: {
-      value: 30,
-      min: -30,
-      max: 30,
-      step: 0.1,
-      onChange: (v) => {
-        ref.current.shadow.camera.right = v
-        ref.current.shadow.camera.updateProjectionMatrix()
-      }
-    },
-    top: {
-      value: 30,
-      min: -30,
-      max: 30,
-      step: 0.1,
-      onChange: (v) => {
-        ref.current.shadow.camera.top = v
-        ref.current.shadow.camera.updateProjectionMatrix()
-      }
-    },
-    bottom: {
-      value: -30,
-      min: -30,
-      max: 30,
-      step: 0.1,
-      onChange: (v) => {
-        ref.current.shadow.camera.bottom = v
-        ref.current.shadow.camera.updateProjectionMatrix()
-      }
-    }
-  })
-
-  // useFrame((state) => {
-  //   const t = state.clock.getElapsedTime() / 5
-  //   ref.current.position.x = Math.cos(t) * 20
-  //   ref.current.position.z = Math.sin(t) * 20
-  // })
-
   return (
     <>
-      <ambientLight intensity={0.1} />
       <directionalLight
-        ref={ref}
+        intensity={1}
         castShadow={true}
-        shadow-bias={-0.003}
-        shadow-mapSize={[2048, 2048]}>
-        <mesh>
-          <sphereBufferGeometry />
-          <meshBasicMaterial />
-        </mesh>
-      </directionalLight>
+        // shadow-bias={-0.0001}
+        shadow-mapSize={[2048, 2048]}
+        position={[65.0, 21.0, 86.0]}
+        shadow-camera-left={-30}
+        shadow-camera-right={30}
+        shadow-camera-top={30}
+        shadow-camera-bottom={-30}
+      />
     </>
   )
 }
 
-function Scene() {
-  const gltf = useGLTF('./models/collision-world.glb')
+function Arena({ controls, lerpEnabled, setLerping, annotations }) {
+  const { scene } = useGLTF('./models/collision-world.glb')
+  const [to, setTo] = useState(new Vector3(10, 10, 10))
+  const [target, setTarget] = useState(new Vector3(0, 1, 0))
+
+  useControls('Camera', {
+    'View A': button(() => {
+      setLerping(true)
+      setTo(new Vector3().copy(annotations.viewA.position))
+      setTarget(new Vector3().copy(annotations.viewA.lookAt))
+    }),
+    'View B': button(() => {
+      setLerping(true)
+      setTo(new Vector3().copy(annotations.viewB.position))
+      setTarget(new Vector3().copy(annotations.viewB.lookAt))
+    }),
+    'View C': button(() => {
+      setLerping(true)
+      setTo(new Vector3().copy(annotations.viewC.position))
+      setTarget(new Vector3().copy(annotations.viewC.lookAt))
+    })
+  })
+
+  useFrame(({ camera }, delta) => {
+    if (lerpEnabled) {
+      //} && camera.position.distanceTo(to) > 0.1) {
+      camera.position.lerp(to, 3 * delta)
+      controls.current.target.lerp(target, 3 * delta)
+    }
+  })
 
   return (
     <>
       <primitive
-        object={gltf.scene}
+        object={scene}
         position={[0, 0, 0]}
         children-0-castShadow={true}
         children-0-receiveShadow={true}
+        children-0-material-envMapIntensity={0.5}
+        onUpdate={() => console.log(scene)}
       />
     </>
   )
 }
 export default function App() {
+  const ref = useRef()
+  const [lerping, setLerping] = useState(false)
+  const annotations = {
+    viewA: {
+      title: 'View A',
+      description: '<p>Looking Down from Above</p>',
+      position: {
+        x: 0,
+        y: 40,
+        z: 0
+      },
+      lookAt: {
+        x: 0,
+        y: 0,
+        z: 0
+      }
+    },
+    viewB: {
+      title: 'View B',
+      position: {
+        x: 8,
+        y: -1.5,
+        z: 2
+      },
+      lookAt: {
+        x: -3,
+        y: 0.15,
+        z: 10
+      }
+    },
+    viewC: {
+      title: 'View C',
+      position: {
+        x: 8,
+        y: 5,
+        z: 11
+      },
+      lookAt: {
+        x: 5,
+        y: -5,
+        z: 10
+      }
+    }
+  }
+
   return (
     <>
-      <Canvas camera={{ position: [10, 10, 10] }} shadows>
+      <Canvas
+        camera={{ position: [10, 10, 10] }}
+        onPointerDown={() => setLerping(false)}
+        shadows>
         <Lights />
-        <Environment preset="dawn" background />
-        <Scene />
-        <OrbitControls target={[0, 1, 0]} />
+        <Environment files="./img/drakensberg_solitary_mountain_1k.hdr" background />
+        <OrbitControls ref={ref} target={[0, 1, 0]} />
+        <Arena
+          controls={ref}
+          lerpEnabled={lerping}
+          setLerping={setLerping}
+          annotations={annotations}
+        />
         <Stats />
       </Canvas>
       <Leva />
