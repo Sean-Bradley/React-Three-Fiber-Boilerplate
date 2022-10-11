11abf1d2b786db96ec82480fb592d60939981af8
Sean Bradley <seanwasere@gmail.com>
2022-10-11 20:19:38 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Stats, OrbitControls, Circle } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { useRef } from 'react'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function App() {
-  const directionalRef = useRef()
   const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
   return (
     <>
       <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-        <directionalLight ref={directionalRef} position={[3.3, 1.0, 4.4]} castShadow={true} />
+        <directionalLight position={[3.3, 1.0, 4.4]} castShadow={true} />
         <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
         <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true}>
           <meshStandardMaterial />
