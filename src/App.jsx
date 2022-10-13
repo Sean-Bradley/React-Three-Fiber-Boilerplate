0370ee02c450e88855e282c22e2ab6e4327a82f5
Sean Bradley <seanwasere@gmail.com>
2022-10-13 22:05:15 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,33 @@
 import { Stats, OrbitControls, useGLTF, Environment } from '@react-three/drei'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useState, useMemo } from 'react'
 import { Leva, useControls, button } from 'leva'
 import { Vector3 } from 'three'
-
-function Lights() {
-  return (
-    <>
-      <directionalLight
-        intensity={1}
-        castShadow={true}
-        shadow-bias={-0.0002}
-        shadow-mapSize={[2048, 2048]}
-        position={[85.0, 80.0, 70.0]}
-        shadow-camera-left={-30}
-        shadow-camera-right={30}
-        shadow-camera-top={30}
-        shadow-camera-bottom={-30}
-      />
-    </>
-  )
-}
+import annotations from './annotations.json';
 
 function Arena({ controls, lerping, setLerping, annotations }) {
   const { scene } = useGLTF('./models/collision-world.glb')
   const [to, setTo] = useState(new Vector3(10, 10, 10))
   const [target, setTarget] = useState(new Vector3(0, 1, 0))
 
-  useControls('Camera', {
-    'View A': button(() => {
-      setTo(annotations.viewA.position)
-      setTarget(annotations.viewA.lookAt)
-      setLerping(true)
-    }),
-    'View B': button(() => {
-      setTo(annotations.viewB.position)
-      setTarget(annotations.viewB.lookAt)
-      setLerping(true)
-    }),
-    'View C': button(() => {
-      setTo(annotations.viewC.position)
-      setTarget(annotations.viewC.lookAt)
-      setLerping(true)
-    })
-  })
+  const buttons = {}
+  useMemo(() => {
+    console.log("in usememo")
+    annotations.forEach(a => {
+      buttons[a.title] = button(() => {
+        setTo(a.position)
+        setTarget(a.lookAt)
+        setLerping(true)
+      })
+    });
+  }, [annotations])
+
+  useControls('Camera', buttons)
 
   useFrame(({ camera }, delta) => {
     if (lerping) {
-      camera.position.lerp(to, 3 * delta)
-      controls.current.target.lerp(target, 3 * delta)
+      camera.position.lerp(to, delta)
+      controls.current.target.lerp(target, delta)
     }
   })
 
@@ -60,60 +39,19 @@ function Arena({ controls, lerping, setLerping, annotations }) {
         receiveShadow
         material-envMapIntensity={0.4}
         onDoubleClick={(e) => {
-          console.log(e)
-          setTo(e.camera.position)
-          setTarget(e.intersections[0].point)
+          setLerping(false)
+          //setTo(e.camera.position.clone())
+          setTarget(e.intersections[0].point.clone())
           setLerping(true)
         }}
       />
     </>
   )
 }
+
 export default function App() {
   const ref = useRef()
   const [lerping, setLerping] = useState(false)
-  const annotations = {
-    viewA: {
-      title: 'View A',
-      description: '<p>Looking Down from Above</p>',
-      position: {
-        x: 0,
-        y: 40,
-        z: 0
-      },
-      lookAt: {
-        x: 0,
-        y: 0,
-        z: 0
-      }
-    },
-    viewB: {
-      title: 'View B',
-      position: {
-        x: 8,
-        y: -1.5,
-        z: 2
-      },
-      lookAt: {
-        x: 8.1,
-        y: -1.5,
-        z: 2
-      }
-    },
-    viewC: {
-      title: 'View C',
-      position: {
-        x: 8,
-        y: 5,
-        z: 11
-      },
-      lookAt: {
-        x: 8,
-        y: 4.9,
-        z: 11
-      }
-    }
-  }
 
   return (
     <>
@@ -121,7 +59,17 @@ export default function App() {
         camera={{ position: [10, 10, 10] }}
         onPointerDown={() => setLerping(false)}
         shadows>
-        <Lights />
+        <directionalLight
+          intensity={1}
+          castShadow={true}
+          shadow-bias={-0.0002}
+          shadow-mapSize={[2048, 2048]}
+          position={[85.0, 80.0, 70.0]}
+          shadow-camera-left={-30}
+          shadow-camera-right={30}
+          shadow-camera-top={30}
+          shadow-camera-bottom={-30}
+        />
         <Environment
           files="./img/drakensberg_solitary_mountain_1k.hdr"
           background
