ec666c8972e7ef19133fa8f33361c4ddc44b073d
Sean Bradley <seanwasere@gmail.com>
2022-10-15 16:18:49 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls, Environment } from '@react-three/drei'
+import { useGLTF, OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import { Leva, useControls } from 'leva'
-
-const MODELS = {
-  hammer: './models/hammer.glb',
-  drill: './models/drill.glb',
-  tapeMeasure: './models/tapeMeasure.glb'
-}
-
-function Model(props) {
-  const { scene } = useGLTF(props.url)
-  return <primitive object={scene} />
-}
+import { Model } from './Shoe-draco'
 
 export default function App() {
-  const dropDown = useControls({
-    model: {
-      value: 'hammer',
-      options: Object.keys(MODELS)
-    }
-  })
-
   return (
     <>
-      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
-        <Environment files="./img/workshop_1k.hdr" background />
-        <group>
-          <Model url={MODELS[dropDown.model]} />
-        </group>
+      <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+        <Environment preset="forest" />
+        <Model />
+        <ContactShadows position={[0, -0.8, 0]} opacity={0.33} />
         <OrbitControls autoRotate />
       </Canvas>
       <Leva />
-      <span id="info">The {dropDown.model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.</span>
     </>
   )
 }
