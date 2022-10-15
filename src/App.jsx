2c9220a3035c7a36299a5aa388698fce3715d903
Sean Bradley <seanwasere@gmail.com>
2022-10-15 12:15:31 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
-import { useMemo } from 'react'
 import { Stats, OrbitControls } from '@react-three/drei'
 
 export default function App() {
-  const polyhedron = useMemo(
-    () => [
-      new THREE.BoxGeometry(),
-      new THREE.SphereGeometry(0.785398),
-      new THREE.DodecahedronGeometry(0.785398)
-    ],
-    []
-  )
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398)
+  ]
 
   return (
     <Canvas camera={{ position: [0, 0, 3] }}>
