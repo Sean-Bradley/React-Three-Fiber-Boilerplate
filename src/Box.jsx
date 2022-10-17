6065a7b42bea946d83245af1ebf0db28c183e5bd
Sean Bradley <seanwasere@gmail.com>
2022-10-17 21:16:46 +0100

    updates

--- src/Box.jsx
+++ src/Box.jsx
@@ -2,7 +2,7 @@ export default function Box(props) {
   return (
     <mesh {...props}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe={true} />
+      <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
   )
 }
