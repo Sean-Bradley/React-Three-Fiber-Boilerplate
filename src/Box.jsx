08ffc06a3e4ae346f767f7a25626dd223ae845df
Sean Bradley <seanwasere@gmail.com>
2022-10-23 15:54:23 +0100

    updates

--- src/Box.jsx
+++ src/Box.jsx
@@ -12,7 +12,7 @@ export default function Box(props) {
     if (rotate) {
       ref.current.rotation.x += 1 * delta
     }
-    ref.current.scale.y = ref.current.scale.z = MathUtils.lerp(ref.current.scale.y, hovered ? 2 : 1, 0.1)
+    ref.current.scale.y = ref.current.scale.z = MathUtils.lerp(ref.current.scale.y, hovered ? 1.2 : 1, 0.1)
     ref.current.material.color.lerp(color.set(hovered ? 0xff0000 : 0x00ff00), 0.1)
   })
 
@@ -26,7 +26,7 @@ export default function Box(props) {
       }}
       onPointerOver={(e) => (e.stopPropagation(), setHover(true))}
       onPointerOut={(e) => (e.stopPropagation(), setHover(false))}>
-      <cylinderGeometry args={[1, 1, .75, 18, 1]} onUpdate={(e) => e.rotateZ(Math.PI / 2)} />
+      <boxGeometry onUpdate={(e) => e.rotateZ(Math.PI / 2)} />
       <meshBasicMaterial wireframe />
       {props.children}
     </mesh>
