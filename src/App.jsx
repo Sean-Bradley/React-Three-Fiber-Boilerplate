49f5b92f74d7c81fbc525997a5e8664c814e8b90
Sean Bradley <seanwasere@gmail.com>
2022-10-26 01:02:22 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
 import Box from './Box'
+import { Stats } from '@react-three/drei'
+import { Vector3 } from 'three'
+
+function Rig() {
+  const { camera, mouse } = useThree()
+  const vec = new Vector3()
+
+  return useFrame(() => {
+    camera.position.lerp(vec.set(mouse.x, mouse.y, camera.position.z), 0.05)
+    camera.lookAt(0, 0, 0)
+  })
+}
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
-  console.log('App count=' + count)
-
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box position={[-0.75, 0, 0]} name="A" parentCount={count} setParentCount={setCount} />
-      <Box position={[0.75, 0, 0]} name="B" parentCount={count} setParentCount={setCount} />
+    <Canvas camera={{ position: [0, 0, 4] }}>
+      <directionalLight position={[1, 1, 1]} />
+      <Box position={[0, 1.5, 0]} name="A0">
+        <Box position={[-0.66, -1, 0]} name="B0">
+          <Box position={[-0.66, -1, 0]} name="C0">
+            <Box position={[-0.66, -1, 0]} name="D0" />
+            <Box position={[0.66, -1, 0]} name="D1" />
+          </Box>
+          <Box position={[0.66, -1, 0]} name="C1">
+            <Box position={[0.66, -1, 0]} name="D2" />
+          </Box>
+        </Box>
+        <Box position={[0.66, -1, 0]} name="B1">
+          <Box position={[0.66, -1, 0]} name="C2">
+            <Box position={[0.66, -1, 0]} name="D3" />
+          </Box>
+        </Box>
+      </Box>
+      <Rig />
+      <Stats />
     </Canvas>
   )
 }
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,27 +1,38 @@
 import { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
+import { Color } from 'three'
+import { useEffect } from 'react'
+import { Html } from '@react-three/drei'
 
-export default function Box({ parentCount, setParentCount, ...props }) {
+export default function Box(props) {
   const ref = useRef()
   const [count, setCount] = useState(0)
+  const black = new Color('black')
 
   console.log('Box ' + props.name + ' count=' + count)
 
-  useFrame((_, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += 0.5 * delta
+  useEffect(() => {
+    ref.current.material.color.set(0x00ff00)
+  })
+
+  useFrame(() => {
+    ref.current.material.color.lerp(black, 0.1)
   })
 
   return (
     <mesh
       {...props}
       ref={ref}
-      onPointerDown={() => {
-        setParentCount(parentCount + 1)
+      onPointerDown={(e) => {
+        //e.stopPropagation()
         setCount(count + 1)
       }}>
       <boxGeometry />
-      <meshBasicMaterial color={'lime'} wireframe />
+      <meshStandardMaterial />
+      <Html position-z={0.5} transform>
+        <span className="counter">{count}</span>
+      </Html>
+      {props.children}
     </mesh>
   )
 }
