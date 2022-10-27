1dea45fef317ee65c1d6796da4e6a398ea2f5a77
Sean Bradley <seanwasere@gmail.com>
2022-10-27 13:39:43 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,10 @@ export default function App() {
 
   return (
     <Canvas camera={{ position: [1, 2, 3] }}>
-      <Polyhedron
-        position={[-0.75, -0.75, 0]}
-        userData-polyhedron={polyhedron}
-      />
-      <Polyhedron
-        position={[0.75, -0.75, 0]}
-        userData-polyhedron={polyhedron}
-      />
-      <Polyhedron
-        position={[-0.75, 0.75, 0]}
-        userData-polyhedron={polyhedron}
-      />
-      <Polyhedron
-        position={[0.75, 0.75, 0]}
-        userData-polyhedron={polyhedron}
-      />
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
       <OrbitControls />
       <axesHelper args={[5]} />
       <gridHelper />
