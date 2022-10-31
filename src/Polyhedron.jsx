4aed620144f99d3e58224f22b986a8c15bd163fc
Sean Bradley <seanwasere@gmail.com>
2022-10-31 09:32:17 +0000

    updates

--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { useControls } from 'leva'
+//import { useControls } from 'leva'
 
 export default function Polyhedron(props) {
   const ref = useRef()
@@ -10,28 +10,28 @@ export default function Polyhedron(props) {
     ref.current.rotation.y += 0.05 * delta
   })
 
-  useControls(
-    props.name,
-    {
-      transparent: {
-        value: false,
-        onChange: (v) => {
-          ref.current.material.transparent = v
-          ref.current.material.needsUpdate = true
-        }
-      },
-      opacity: {
-        value: 0.6,
-        min: 0,
-        max: 1,
-        step: 0.01,
-        onChange: (v) => {
-          ref.current.material.opacity = v
-        }
-      }
-    },
-    { collapsed: true }
-  )
+  // useControls(
+  //   props.name,
+  //   {
+  //     transparent: {
+  //       value: false,
+  //       onChange: (v) => {
+  //         ref.current.material.transparent = v
+  //         ref.current.material.needsUpdate = true
+  //       }
+  //     },
+  //     opacity: {
+  //       value: 0.6,
+  //       min: 0,
+  //       max: 1,
+  //       step: 0.01,
+  //       onChange: (v) => {
+  //         ref.current.material.opacity = v
+  //       }
+  //     }
+  //   },
+  //   { collapsed: true }
+  // )
 
   return (
     <mesh {...props} ref={ref} castShadow>
