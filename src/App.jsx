be78aa4cecef5d2ead71a63b3b8931ed4da1928f
Sean Bradley <seanwasere@gmail.com>
2022-11-01 22:53:40 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,13 @@ export default function App() {
   const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
   return (
-    <>
-      <Canvas camera={{ position: [-0.5, 1, 2] }}>
-        <Environment files="./img/venice_sunset_1k.hdr" background />
-        <directionalLight position={[3.3, 1.0, 4.4]} />
-        <primitive object={gltf.scene} position={[0, 1, 0]} />
-        <OrbitControls target={[0, 1, 0]} autoRotate />
-        <axesHelper args={[5]} />
-        <Stats />
-      </Canvas>
-    </>
+    <Canvas camera={{ position: [-0.5, 1, 2] }}>
+      <Environment files="./img/venice_sunset_1k.hdr" background blur={0.5} />
+      <directionalLight position={[3.3, 1.0, 4.4]} />
+      <primitive object={gltf.scene} position={[0, 1, 0]} />
+      <OrbitControls target={[0, 1, 0]} autoRotate />
+      <axesHelper args={[5]} />
+      <Stats />
+    </Canvas>
   )
 }
