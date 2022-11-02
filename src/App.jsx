80b75a00097752c1b3547ac8fabe1ec7763b558d
Sean Bradley <seanwasere@gmail.com>
2022-11-02 11:36:47 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useLoader } from '@react-three/fiber'
 import { useMemo } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Canvas } from '@react-three/fiber'
-import { Leva, useControls } from 'leva'
+import { useControls } from 'leva'
 
 function Model() {
   const { scene, materials } = useLoader(GLTFLoader, './models/scene.glb')
@@ -21,40 +21,35 @@ function Model() {
   })
 
   return (
-    <>
-      <primitive
-        object={scene}
-        children-0-children-0-castShadow={true}
-        children-1-receiveShadow={true}
-        children-2-castShadow={true}
-        children-3-castShadow={true}
-        children-4-castShadow={true}
-        children-5-castShadow={true}
-        children-6-castShadow={true}
-        children-7-castShadow={true}
-        children-7-rotation={[options.x, options.y, options.z]}
-        children-7-visible={options.visible}
-        children-7-material-color={options.color}
-      />
-    </>
+    <primitive
+      object={scene}
+      children-0-children-0-castShadow={true}
+      children-1-receiveShadow={true}
+      children-2-castShadow={true}
+      children-3-castShadow={true}
+      children-4-castShadow={true}
+      children-5-castShadow={true}
+      children-6-castShadow={true}
+      children-7-castShadow={true}
+      children-7-rotation={[options.x, options.y, options.z]}
+      children-7-visible={options.visible}
+      children-7-material-color={options.color}
+    />
   )
 }
 
 export default function App() {
   return (
-    <>
-      <Canvas
-        camera={{ position: [0, 0.5, 3] }}
-        shadows
-        onCreated={(state) => {
-          state.gl.physicallyCorrectLights = true
-        }}>
-        <Environment files="./img/venice_sunset_1k.hdr" />
-        <Model />
-        <OrbitControls target={[0, 1, 0]} autoRotate />
-      </Canvas>
+    <Canvas
+      camera={{ position: [0, 0.5, 3] }}
+      shadows
+      onCreated={(state) => {
+        state.gl.physicallyCorrectLights = true
+      }}>
+      <Environment files="./img/venice_sunset_1k.hdr" />
+      <Model />
+      <OrbitControls target={[0, 1, 0]} autoRotate />
       <Stats />
-      <Leva />
-    </>
+    </Canvas>
   )
 }
