326a5e335a91576efa3e847ad5a5500649cd1d6e
Sean Bradley <seanwasere@gmail.com>
2022-11-02 14:57:22 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,77 @@
-import { Stats, OrbitControls, Environment } from '@react-three/drei'
+import {
+  Stats,
+  OrbitControls,
+  Environment,
+  ContactShadows
+} from '@react-three/drei'
 import { useLoader } from '@react-three/fiber'
-import { useMemo } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Canvas } from '@react-three/fiber'
 import { useControls } from 'leva'
 
 function Model() {
-  const { scene, materials } = useLoader(GLTFLoader, './models/scene.glb')
+  const { scene } = useLoader(GLTFLoader, './models/scene.glb')
 
-  useMemo(() => {
-    materials.Material.envMapIntensity = 0.5
-  }, [materials])
-
-  const options = useControls('Suzanne', {
+  const {
+    x,
+    y,
+    z,
+    visible,
+    color,
+    metalness,
+    roughness,
+    clearcoat,
+    clearcoatRoughness,
+    transmission,
+    ior,
+    thickness
+  } = useControls('Suzanne', {
     x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
     y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
     z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
     visible: true,
-    color: { value: 'red' }
+    color: { value: '#ffbc85' },
+    metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
+    clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
+    ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
+    thickness: { value: 3.12, min: 0, max: 5, step: 0.01 }
   })
 
   return (
     <primitive
       object={scene}
-      children-0-children-0-castShadow={true}
-      children-1-receiveShadow={true}
-      children-2-castShadow={true}
-      children-3-castShadow={true}
-      children-4-castShadow={true}
-      children-5-castShadow={true}
-      children-6-castShadow={true}
-      children-7-castShadow={true}
-      children-7-rotation={[options.x, options.y, options.z]}
-      children-7-visible={options.visible}
-      children-7-material-color={options.color}
+      children-0-rotation={[x, y, z]}
+      children-0-visible={visible}
+      children-0-material-color={color}
+      children-0-material-metalness={metalness}
+      children-0-material-roughness={roughness}
+      children-0-material-clearcoat={clearcoat}
+      children-0-material-clearcoatRoughness={clearcoatRoughness}
+      children-0-material-transmission={transmission}
+      children-0-material-ior={ior}
+      children-0-material-thickness={thickness}
+    />
+  )
+}
+
+function Env() {
+  const { height, radius, scale } = useControls('Ground', {
+    height: { value: 10, min: 0, max: 100, step: 1 },
+    radius: { value: 115, min: 0, max: 1000, step: 1 },
+    scale: { value: 100, min: 0, max: 1000, step: 1 }
+  })
+  return (
+    <Environment
+      preset="sunset"
+      background
+      ground={{
+        height: height,
+        radius: radius,
+        scale: scale
+      }}
     />
   )
 }
@@ -41,14 +79,14 @@ function Model() {
 export default function App() {
   return (
     <Canvas
-      camera={{ position: [0, 0.5, 3] }}
-      shadows
+      camera={{ position: [0, 6, 12] }}
       onCreated={(state) => {
         state.gl.physicallyCorrectLights = true
       }}>
-      <Environment files="./img/venice_sunset_1k.hdr" />
+      <Env />
       <Model />
-      <OrbitControls target={[0, 1, 0]} autoRotate />
+      <ContactShadows scale={150} position={[0.33, -0.33, 0.33]} opacity={1.5} />
+      <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
       <Stats />
     </Canvas>
   )
