49d7ae2c6ad67f4d5a2affae51f01c02d7aeda48
Sean Bradley <seanwasere@gmail.com>
2022-11-19 23:50:27 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Stats, OrbitControls } from '@react-three/drei'
 export default function App() {
   return (
     <Canvas camera={{ position: [1, 2, 3] }}>
-      <Box />
+      <Box position={[0, 0.5, 0]} />
       <OrbitControls />
       <axesHelper args={[5]} />
       <gridHelper />
