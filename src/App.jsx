a83038b94c440ff80e06893f5edd456220df0b82
Sean Bradley <seanwasere@gmail.com>
2022-11-27 11:26:31 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { Stats, OrbitControls } from '@react-three/drei'
 export default function App() {
   return (
     <Canvas camera={{ position: [1, 2, 3] }}>
-      <Box position={[0, 0.5, 0]} />
+      <Box position={[-1.5, 0.5, 0]} />
+      <Box position={[0, 0.5, 0]} selected/>
+      <Box position={[1.5, 0.5, 0]} />
       <OrbitControls />
       <axesHelper args={[5]} />
       <gridHelper />
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,22 +1,23 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 import useKeyboard from './useKeyboard'
 
 export default function Box(props) {
   const ref = useRef()
   const keyMap = useKeyboard()
+  const [selected, setSelected] = useState(props.selected)
 
   useFrame((_, delta) => {
-    keyMap['KeyA'] && (ref.current.position.x -= 1 * delta)
-    keyMap['KeyD'] && (ref.current.position.x += 1 * delta)
-    keyMap['KeyW'] && (ref.current.position.z -= 1 * delta)
-    keyMap['KeyS'] && (ref.current.position.z += 1 * delta)
+    keyMap['KeyA'] && selected && (ref.current.position.x -= 1 * delta)
+    keyMap['KeyD'] && selected && (ref.current.position.x += 1 * delta)
+    keyMap['KeyW'] && selected && (ref.current.position.z -= 1 * delta)
+    keyMap['KeyS'] && selected && (ref.current.position.z += 1 * delta)
   })
 
   return (
-    <mesh ref={ref} {...props}>
+    <mesh ref={ref} {...props} onPointerDown={() => setSelected(!selected)}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshBasicMaterial color={0x00ff00} wireframe={!selected} />
     </mesh>
   )
 }
