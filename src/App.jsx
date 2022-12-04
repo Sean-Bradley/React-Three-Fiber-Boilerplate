2970c4fcd7d672ed2704aa21a0e76eb633b8627a
Sean Bradley <seanwasere@gmail.com>
2022-12-04 21:33:45 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,38 @@
 import { Canvas } from '@react-three/fiber'
 import { useMemo, useState } from 'react'
-import { Stats, OrbitControls, Environment } from '@react-three/drei'
+import { Stats, OrbitControls, Environment, Bounds, useBounds } from '@react-three/drei'
 import { useControls } from 'leva'
-import { SphereGeometry } from 'three'
+import { OctahedronGeometry, Vector3, MathUtils } from 'three'
 
 function Squircle() {
-  const [segments, setSegments] = useState(32)
+  const [n, setN] = useState(2)
+  const bounds = useBounds()
 
   const geometry = useMemo(() => {
-    const g = new SphereGeometry(1, segments, segments)
-    const p = g.attributes.position
-    const a = p.array
+    const g = new OctahedronGeometry(1, 16)
+    const p = g.attributes.position.array
 
-    for (let i = 1; i < p.count; i++) {
-      let x = a[3 * i],
-        y = a[3 * i + 1],
-        z = a[3 * i + 2],
-        e
-      do {
-        e = x ** 4 + y ** 4 + z ** 4 - 1
-        if (e > 0) {
-          x *= 0.9999
-          y *= 0.9999
-          z *= 0.9999
-        } else {
-          x *= 1.0001
-          y *= 1.0001
-          z *= 1.0001
-        }
-      } while (Math.abs(e) > 1e-7)
-      a[3 * i] = x
-      a[3 * i + 1] = y
-      a[3 * i + 2] = z
+    for (let i = 0; i < p.length; i += 3) {
+      const v = new Vector3(p[i], p[i + 1], p[i + 2])
+      v.x = Math.tanh(v.x)
+      v.y = Math.tanh(v.y)
+      v.z = Math.tanh(v.z)
+      p[i] = MathUtils.lerp(p[i], v.x, n)
+      p[i + 1] = MathUtils.lerp(p[i + 1], v.y, n)
+      p[i + 2] = MathUtils.lerp(p[i + 2], v.z, n)
     }
     return g
-  }, [segments])
+  }, [n])
 
   useControls('Squircle', {
     segments: {
-      value: 32,
-      min: 6,
-      max: 32,
-      step: 1,
+      value: 2,
+      min: -64,
+      max: 64,
+      step: 0.1,
       onChange: (v) => {
-        setSegments(v)
+        setN(v)
+        bounds.refresh().fit()
       }
     }
   })
@@ -57,9 +46,11 @@ function Squircle() {
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [1.6, 1.3, -2.5] }}>
+    <Canvas camera={{ position: [0, 2, -1.5] }}>
       <Environment files="./img/rustig_koppie_puresky_1k.hdr" background />
-      <Squircle />
+      <Bounds fit margin={1.8} damping={10}>
+        <Squircle />
+      </Bounds>
       <OrbitControls target={[0, 1, 0]} autoRotate />
       <Stats />
     </Canvas>
