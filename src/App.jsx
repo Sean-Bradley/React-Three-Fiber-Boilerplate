dc7f61cc3394556e8a295b5869afba7d950306d7
Sean Bradley <seanwasere@gmail.com>
2022-12-04 09:53:16 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
-import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
+import { useRef, useMemo } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { Stats } from '@react-three/drei'
+import useKeyboard from './useKeyboard'
+import { Vector3, Quaternion } from 'three'
 
-export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398)
-  ]
+function Ball({ floor }) {
+  console.log('creating ball')
+  const ref = useRef()
+  const keyMap = useKeyboard()
+
+  const v0 = useMemo(() => new Vector3(), [])
+  const q = useMemo(() => new Quaternion(), [])
+  const angularVelocity = useMemo(() => new Vector3(), [])
+
+  useFrame((_, delta) => {
+    keyMap['KeyW'] && (angularVelocity.x -= delta * 5)
+    keyMap['KeyS'] && (angularVelocity.x += delta * 5)
+    keyMap['KeyA'] && (angularVelocity.z += delta * 5)
+    keyMap['KeyD'] && (angularVelocity.z -= delta * 5)
+
+    q.setFromAxisAngle(angularVelocity, delta).normalize()
+    ref.current.applyQuaternion(q)
+
+    angularVelocity.lerp(v0, 0.01) // slow down the roll
+
+    floor.current.position.x += angularVelocity.z * delta
+    floor.current.position.z -= angularVelocity.x * delta
+
+    floor.current.position.x = floor.current.position.x % 10
+    floor.current.position.z = floor.current.position.z % 10
+  })
 
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
-      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    <mesh ref={ref} position-y={1.0}>
+      <sphereGeometry />
+      <meshNormalMaterial wireframe />
+    </mesh>
+  )
+}
+
+export default function App() {
+  const ref = useRef()
+  return (
+    <Canvas
+      camera={{ position: [0, 2.5, 2.5] }}
+      onCreated={({ camera }) => camera.lookAt(0, 1, 0)}>
+      <gridHelper ref={ref} args={[100, 100]} />
+      <Ball floor={ref} />
       <Stats />
     </Canvas>
   )
---
+++ src/useKeyboard.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react'
+
+export default function useKeyboard() {
+  const keyMap = useRef({})
+
+  useEffect(() => {
+    const onDocumentKey = (e) => {
+      keyMap.current[e.code] = e.type === 'keydown'
+    }
+    document.addEventListener('keydown', onDocumentKey)
+    document.addEventListener('keyup', onDocumentKey)
+    return () => {
+      document.removeEventListener('keydown', onDocumentKey)
+      document.removeEventListener('keyup', onDocumentKey)
+    }
+  })
+
+  return keyMap.current
+}
