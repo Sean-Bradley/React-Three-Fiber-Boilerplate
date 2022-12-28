ab7fa8aace254167bba902da42b3b65c2f7d6cd4
Sean Bradley <seanwasere@gmail.com>
2022-12-28 12:55:59 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
-import { Model } from './Shoe'
+import { OrbitControls, Environment } from '@react-three/drei'
+import { Room } from './Room'
+import { ArmChair } from './ArmChair'
+import { useState } from 'react'
 
 export default function App() {
-  console.log("in app")
+  const [selected, setSelected] = useState(0)
+
   return (
-    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
-      <Environment preset="forest" />
-      <Model />
-      <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
-      <OrbitControls autoRotate />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [2.25, 1, 2.25] }}>
+        <Environment
+          preset="forest"
+          background
+          ground={{
+            height: 2,
+            radius: 115,
+            scale: 100
+          }}
+        />
+        <directionalLight
+          position={[5, 1.5, 3]}
+          intensity={2}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-bias={-0.0001}
+        />
+        <Room />
+        <ArmChair selected={selected} />
+        <OrbitControls target={[1.5, 0.8, 1.5]} minPolarAngle={0} maxPolarAngle={Math.PI / 2 + Math.PI / 12} />
+      </Canvas>
+      <input id="btn0" className="btn" type="image" src="./img/fabric_pattern_05.png" alt="Fabric 0" onClick={() => setSelected(0)} />
+      <input id="btn1" className="btn" type="image" src="./img/leather_red.png" alt="Fabric 1" onClick={() => setSelected(1)} />
+      <input id="btn2" className="btn" type="image" src="./img/fabric_pattern_7.png" alt="Fabric 2" onClick={() => setSelected(2)} />
+      <input id="btn3" className="btn" type="image" src="./img/book_pattern.png" alt="Fabric 3" onClick={() => setSelected(3)} />
+      <input id="btn4" className="btn" type="image" src="./img/denim_fabric_02.png" alt="Fabric 4" onClick={() => setSelected(4)} />
+    </>
   )
 }
---
+++ src/Room.jsx
@@ -0,0 +1,30 @@
+import { useGLTF } from '@react-three/drei'
+
+export function Room() {
+  const { nodes, materials } = useGLTF('./models/room-transformed.glb')
+
+  return (
+    <>
+      <group dispose={null} position-y={0.3}>
+        <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <mesh geometry={nodes.room_1.geometry} material={materials.ground_1} receiveShadow />
+          <mesh geometry={nodes.room_2.geometry} material={materials.wall_1_2} castShadow receiveShadow />
+          <mesh geometry={nodes.room_3.geometry} material={materials.room_5_30} receiveShadow />
+          <mesh geometry={nodes.room_4.geometry} material={materials.white} castShadow receiveShadow />
+          <mesh
+            geometry={nodes.room_5.geometry}
+            material={materials.flltgrey}
+            material-transparent={true}
+            material-opacity={0.1}
+          />
+          <mesh geometry={nodes.room_6.geometry} material={materials.dkgrey} receiveShadow />
+          <mesh geometry={nodes.room_7.geometry} material={materials.amber} castShadow />
+          <mesh geometry={nodes.room_8.geometry} material={materials.yellow_green} castShadow />
+          <mesh geometry={nodes.room_9.geometry} material={materials.flbrown} castShadow />
+        </group>
+      </group>
+    </>
+  )
+}
+
+useGLTF.preload('./models/room-transformed.glb')
