38511d5f84bf59081b1aa5c46eecd51658fe7c54
Sean Bradley <seanwasere@gmail.com>
2022-12-28 23:24:38 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,16 @@ export default function App() {
             scale: 100
           }}
         />
-        <directionalLight
-          position={[5, 1.5, 3]}
-          intensity={2}
-          castShadow
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-bias={-0.0001}
-        />
+        <directionalLight position={[5, 1.5, 3]} intensity={2} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} shadow-bias={-0.0001} />
         <Room />
         <ArmChair selected={selected} />
         <OrbitControls target={[1.5, 0.8, 1.5]} minPolarAngle={0} maxPolarAngle={Math.PI / 2 + Math.PI / 12} />
       </Canvas>
-      <input id="btn0" className="btn" type="image" src="./img/fabric_pattern_05.png" alt="Fabric 0" onClick={() => setSelected(0)} />
-      <input id="btn1" className="btn" type="image" src="./img/leather_red.png" alt="Fabric 1" onClick={() => setSelected(1)} />
-      <input id="btn2" className="btn" type="image" src="./img/fabric_pattern_7.png" alt="Fabric 2" onClick={() => setSelected(2)} />
-      <input id="btn3" className="btn" type="image" src="./img/book_pattern.png" alt="Fabric 3" onClick={() => setSelected(3)} />
-      <input id="btn4" className="btn" type="image" src="./img/denim_fabric_02.png" alt="Fabric 4" onClick={() => setSelected(4)} />
+      <input id="btn0" className="btn" type="image" src="./img/fabric_pattern_05.png" alt="Fabric 0" onPointerDown={() => setSelected(0)} draggable={false} />
+      <input id="btn1" className="btn" type="image" src="./img/leather_red.png" alt="Fabric 1" onPointerDown={() => setSelected(1)} draggable={false} />
+      <input id="btn2" className="btn" type="image" src="./img/fabric_pattern_7.png" alt="Fabric 2" onPointerDown={() => setSelected(2)} draggable={false} />
+      <input id="btn3" className="btn" type="image" src="./img/book_pattern.png" alt="Fabric 3" onPointerDown={() => setSelected(3)} draggable={false} />
+      <input id="btn4" className="btn" type="image" src="./img/denim_fabric_02.png" alt="Fabric 4" onPointerDown={() => setSelected(4)} draggable={false} />
     </>
   )
 }
