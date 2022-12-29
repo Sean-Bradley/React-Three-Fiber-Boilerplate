e53cd20705329da243da0dfb8720d4816a8a32bc
Sean Bradley <seanwasere@gmail.com>
2022-12-29 21:03:22 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { OrbitControls, Environment, Hud, OrthographicCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Environment } from '@react-three/drei'
 import { Room } from './Room'
 import { ArmChair } from './ArmChair'
-import { useRef, useState } from 'react'
-import { TextureLoader, MathUtils } from 'three'
-
-function Button({ id, texture, position, setSelected }) {
-  const ref = useRef()
-  const [hovered, setHovered] = useState(false)
-  useFrame(() => {
-    ref.current.scale.y = ref.current.scale.x = MathUtils.lerp(ref.current.scale.y, hovered ? 1.5 : 1, 0.25)
-  })
-  return (
-    <mesh ref={ref} position={position} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} onPointerDown={() => setSelected(id)}>
-      <circleGeometry />
-      <meshBasicMaterial map={texture} />
-    </mesh>
-  )
-}
-
-function MaterialMenu({ setSelected }) {
-  const texture = useLoader(TextureLoader, [
-    './img/fabric_pattern_05.png',
-    './img/leather_red.png',
-    './img/fabric_pattern_7.png',
-    './img/book_pattern.png',
-    './img/denim_fabric_02.png'
-  ])
-
-  return (
-    <Hud>
-      <OrthographicCamera makeDefault position={[0, 0, 0.1]} zoom={50} />
-      <Button id={0} texture={texture[0]} position={[-6, -6, 0]} setSelected={setSelected} />
-      <Button id={1} texture={texture[1]} position={[-3, -6, 0]} setSelected={setSelected} />
-      <Button id={2} texture={texture[2]} position={[-0, -6, 0]} setSelected={setSelected} />
-      <Button id={3} texture={texture[3]} position={[3, -6, 0]} setSelected={setSelected} />
-      <Button id={4} texture={texture[4]} position={[6, -6, 0]} setSelected={setSelected} />
-    </Hud>
-  )
-}
 
 export default function App() {
-  const [selected, setSelected] = useState(0)
-
   return (
     <Canvas shadows camera={{ position: [2.25, 1, 2.25] }}>
       <Environment
@@ -56,9 +17,8 @@ export default function App() {
       />
       <directionalLight position={[5, 1.5, 3]} intensity={2} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} shadow-bias={-0.0001} />
       <Room />
-      <ArmChair selected={selected} />
+      <ArmChair />
       <OrbitControls target={[1.5, 0.8, 1.5]} minPolarAngle={0} maxPolarAngle={Math.PI / 2 + Math.PI / 12} />
-      <MaterialMenu setSelected={setSelected} />
     </Canvas>
   )
 }
--- src/ArmChair.jsx
+++ src/ArmChair.jsx
@@ -1,8 +1,46 @@
-import { useGLTF } from '@react-three/drei'
-import { useMemo } from 'react'
-import { useRef } from 'react'
+import { useGLTF, Hud, OrthographicCamera } from '@react-three/drei'
+import { useFrame, useLoader } from '@react-three/fiber'
+import { useMemo, useRef, useState } from 'react'
+import { TextureLoader, MathUtils } from 'three'
 
-export function ArmChair({ selected }) {
+function Button({ id, texture, position, setSelected }) {
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
+  useFrame(() => {
+    ref.current.scale.y = ref.current.scale.x = MathUtils.lerp(ref.current.scale.y, hovered ? 1.5 : 1, 0.25)
+  })
+  return (
+    <mesh ref={ref} position={position} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} onPointerDown={() => setSelected(id)}>
+      <circleGeometry />
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  )
+}
+
+function MaterialMenu({ setSelected }) {
+  const texture = useLoader(TextureLoader, [
+    './img/fabric_pattern_05.png',
+    './img/leather_red.png',
+    './img/fabric_pattern_7.png',
+    './img/book_pattern.png',
+    './img/denim_fabric_02.png'
+  ])
+
+  return (
+    <Hud>
+      <OrthographicCamera makeDefault position={[0, 0, 0.1]} zoom={50} />
+      <Button id={0} texture={texture[0]} position={[-6, -6, 0]} setSelected={setSelected} />
+      <Button id={1} texture={texture[1]} position={[-3, -6, 0]} setSelected={setSelected} />
+      <Button id={2} texture={texture[2]} position={[-0, -6, 0]} setSelected={setSelected} />
+      <Button id={3} texture={texture[3]} position={[3, -6, 0]} setSelected={setSelected} />
+      <Button id={4} texture={texture[4]} position={[6, -6, 0]} setSelected={setSelected} />
+    </Hud>
+  )
+}
+
+export function ArmChair() {
+  const [selected, setSelected] = useState(0)
+
   const ref = useRef()
   const { nodes, materials } = useGLTF('./models/armchair-transformed.glb')
 
@@ -24,6 +62,7 @@ export function ArmChair({ selected }) {
           <mesh geometry={nodes.armchair001_3.geometry} material={materials.wooden_legs} castShadow />
         </group>
       </group>
+      <MaterialMenu setSelected={setSelected} />
     </>
   )
 }
