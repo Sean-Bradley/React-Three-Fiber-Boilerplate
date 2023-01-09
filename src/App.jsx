78258cbbf083c40174156c29819d9d9658b9ae4b
Sean Bradley <seanwasere@gmail.com>
2023-01-09 23:34:23 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { Canvas } from '@react-three/fiber'
-import { Stats } from '@react-three/drei'
+import { Stats, useProgress, Html } from '@react-three/drei'
 import Game from './Game'
 import { Physics } from '@react-three/cannon'
+import { Suspense } from 'react'
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
 
 export default function App() {
   return (
     <>
       <Canvas shadows onPointerDown={(e) => e.target.requestPointerLock()}>
-        <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} />
-        <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} />
-        <Physics>
-          <Game />
-        </Physics>
-        <gridHelper />
-        <Stats />
+        <Suspense fallback={<Loader />}>
+          <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} />
+          <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} />
+          <Physics>
+            <Game />
+          </Physics>
+          <gridHelper />
+          <Stats />
+        </Suspense>
       </Canvas>
       <div id="instructions">
-        WASD to move and SPACE to jump. Model from{' '}
+        WASD to move
+        <br />
+        SPACE to jump.
+        <br />
+        Model from{' '}
         <a href="https://www.mixamo.com" target="_blank" rel="nofollow noreferrer">
           Mixamo
         </a>
