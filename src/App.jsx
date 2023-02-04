8e36c456e5f28e082c324d42127051a7ce572d81
Sean Bradley <seanwasere@gmail.com>
2023-02-04 11:43:08 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { Canvas } from '@react-three/fiber'
-import Box from './Box'
+import { useRef, forwardRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+const Box = forwardRef(function Box(props, ref) {
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta
+    ref.current.rotation.y += 0.5 * delta
+  })
+
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+})
 
 export default function App() {
+  const ref = useRef()
+
   return (
     <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box position={[-0.75, 0, 0]} name="A"/>
-      <Box position={[0.75, 0, 0]} name="B"/>
+      <Box ref={ref} />
     </Canvas>
   )
 }
