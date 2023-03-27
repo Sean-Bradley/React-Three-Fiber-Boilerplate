c721f55be1c2fdf4fa694c650285edd0be409a6d
Sean Bradley <seanwasere@gmail.com>
2023-03-27 09:07:57 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,109 @@
-import { Stats, OrbitControls } from '@react-three/drei'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Stats, OrbitControls, Environment, Text } from '@react-three/drei'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { useRef } from 'react'
-import { Debug, Physics, useBox, usePlane } from '@react-three/cannon'
 import { useControls } from 'leva'
-
-function Plane(props) {
-  usePlane(() => ({ ...props }))
-}
-
-function InstancedBoxes() {
-  const [ref, { at }] = useBox(
-    (i) => ({
-      args: [1, 1, 1],
-      type: 'Dynamic',
-      position: [Math.floor(i % 8) * 1.01 - 4, Math.floor((i / 64) % 64) * 1.01 + 4, Math.floor((i / 8) % 8) * 1.01 - 4]
-    }),
-    useRef()
-  )
-
-  useFrame(() => {
-    console.log(at(0))
-  })
-
-  return (
-    <instancedMesh
-      ref={ref}
-      args={[undefined, undefined, 512]}
-      onPointerDown={(e) => {
-        e.stopPropagation()
-        at(e.instanceId).mass.set(1)
-      }}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshNormalMaterial />
-    </instancedMesh>
-  )
-}
+import { TextureLoader } from 'three'
+import { Model } from './Test'
+import { Perf } from 'r3f-perf'
+import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
 
 export default function App() {
-  const gravity = useControls('Gravity', {
-    x: { value: 0, min: -10, max: 10, step: 0.1 },
-    y: { value: -9.8, min: -10, max: 10, step: 0.1 },
-    z: { value: 0, min: -10, max: 10, step: 0.1 }
+  const { focusDistance, focalLength, bokehScale } = useControls({
+    focusDistance: {
+      min: 0,
+      max: 4,
+      value: 2
+    },
+    focalLength: {
+      min: 0,
+      max: 1,
+      value: 0.1
+    },
+    bokehScale: {
+      min: 0,
+      max: 10,
+      value: 2
+    }
   })
-
   return (
-    <Canvas camera={{ position: [6, 9, 9] }}>
-      <Physics gravity={[gravity.x, gravity.y, gravity.z]} broadphase="SAP">
-        <Debug color={0x004400}>
-          <Plane rotation={[-Math.PI / 2, 0, 0]} />
-        </Debug>
-        <InstancedBoxes />
-      </Physics>
-      <OrbitControls target-y={5} />
-      <Stats />
+    <Canvas camera={{ position: [0, 0, 2], far: 100000 }} gl={{ logarithmicDepthBuffer: true }}>
+      <Perf position="top-left" />
+      <Environment
+        // preset="sunset"
+        files="./img/industrial_sunset_02_1k.hdr"
+        background
+      />
+      <Model />
+      <OrbitControls />
+      <EffectComposer>
+        <DepthOfField focusDistance={focusDistance} focalLength={focalLength} bokehScale={bokehScale} />
+      </EffectComposer>
     </Canvas>
   )
 }
+
+// export default function App() {
+//   // const xRef = useRef()
+//   const texture = useLoader(TextureLoader, './img/grid.png')
+
+//   const rotation = useControls({
+//     x: {
+//       value: 0,
+//       min: 0,
+//       max: Math.PI,
+//       step: 0.001
+//     },
+//     y: {
+//       value: 0,
+//       min: 0,
+//       max: Math.PI,
+//       step: 0.001
+//     },
+//     z: {
+//       value: 0,
+//       min: 0,
+//       max: Math.PI,
+//       step: 0.001
+//     }
+//   })
+
+//   return (
+//     <Canvas camera={{ position: [0, 0, 2] }}>
+//       <group rotation-z={rotation.x}>
+//         <mesh>
+//           <sphereGeometry />
+//           <meshNormalMaterial wireframe={true} />
+//         </mesh>
+//         <mesh rotation-x={-Math.PI / 2}>
+//           <cylinderGeometry args={[0.025, 0.025, 2]} />
+//           <meshNormalMaterial />
+//         </mesh>
+//         <mesh>
+//           <cylinderGeometry args={[0.025, 0.025, 2]} />
+//           <meshNormalMaterial />
+//         </mesh>
+//         <mesh rotation-z={-Math.PI / 2}>
+//           <cylinderGeometry args={[0.025, 0.025, 2]} />
+//           <meshNormalMaterial />
+//         </mesh>
+//         <Text position-z={1.01}>x</Text>
+//         <Text position-z={-1.01}>x</Text>
+//         <Text position-y={1.01} rotation-x={-Math.PI / 2}>
+//           y
+//         </Text>
+//         <Text position-y={-1.01} rotation-x={Math.PI / 2}>
+//           y
+//         </Text>
+//         <Text position-x={1.01} rotation-y={Math.PI / 2}>
+//           z
+//         </Text>
+//         <Text position-x={-1.01} rotation-y={-Math.PI / 2}>
+//           z
+//         </Text>
+//       </group>
+
+//       <OrbitControls />
+//       <Stats />
+//     </Canvas>
+//   )
+// }
