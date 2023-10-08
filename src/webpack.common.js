ce7d42cb57f4089f93e941ee312d833f22a1d471
Sean Bradley <seanwasere@gmail.com>
2023-10-08 09:55:09 +0100

    updates

--- src/webpack.common.js
+++ src/webpack.common.js
@@ -26,6 +26,6 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../public'),
+        path: path.resolve(__dirname, '../build'),
     },
 }
