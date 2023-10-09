04178db99d18ba565645a08409a0d073028c9c6a
Sean Bradley <seanwasere@gmail.com>
2023-10-09 12:40:40 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ export default function App() {
     <>
       <Canvas shadows onPointerDown={(e) => e.target.requestPointerLock()}>
         <Suspense fallback={<Loader />}>
-          <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 25} />
-          <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 25} />
+          <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
+          <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
           <Physics>
             <Game />
           </Physics>
