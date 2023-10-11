f037d2a3e792434384dc1f0e989eea8aef355767
Sean Bradley <seanwasere@gmail.com>
2023-10-11 13:39:02 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
+import {
+  Stats,
+  OrbitControls,
+  OrthographicCamera,
+  Environment
+} from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
 
 export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398)
-  ]
-
+  const rtop = useLoader(THREE.TextureLoader, './img/rtop.jpg')
+  const rleft = useLoader(THREE.TextureLoader, './img/rleft.jpg')
+  const rright = useLoader(THREE.TextureLoader, './img/rright.jpg')
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
-      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    <Canvas camera={{ position: [1, 1, 1] }}>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial
+          attach="material-0"
+          map={rright}
+          map-repeat={0.5}
+          map-offset-x={0.5}
+        />
+        <meshBasicMaterial attach="material-1" color="orange" />
+        <meshStandardMaterial
+          attach="material-2"
+          map={rtop}
+          map-repeat={0.5}
+          map-offset-y={0.5}
+          // map-center={(0.5, 0.5)}
+          // map-rotation={Math.PI/4}
+        />
+        <meshBasicMaterial attach="material-3" color="orange" />
+        <meshStandardMaterial
+          attach="material-4"
+          map={rleft}
+          map-repeat={0.5}
+          map-offset-y={1}
+          // map-wrapS={THREE.RepeatWrapping}
+          map-wrapT={THREE.RepeatWrapping}
+        />
+        <meshBasicMaterial attach="material-5" color="orange" />
+      </mesh>
+      <ambientLight color="blue"></ambientLight>
+      <Environment preset="forest"></Environment>
+      <OrthographicCamera makeDefault position={[1, 1, 1]} zoom={400} />
       <OrbitControls />
       <Stats />
     </Canvas>
