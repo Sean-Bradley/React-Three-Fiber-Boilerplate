a48f25e63a411846df6c61413bdf8b8dbc554576
Sean Bradley <seanwasere@gmail.com>
2023-10-16 14:14:12 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,34 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import {
   OrbitControls,
   Environment,
-  ContactShadows,
   useProgress,
   Html
 } from '@react-three/drei'
 import { Model } from './Scan'
 import { Suspense, useRef } from 'react'
-import { create } from 'zustand'
-import { Vector3 } from 'three'
-
-export const useStore = create((set) => ({
-  target: new Vector3(),
-  lerping: false,
-  setLerping: (v) => set({ lerping: v })
-}))
+import TWEEN from '@tweenjs/tween.js'
 
 function Loader() {
   const { progress } = useProgress()
   return <Html center>{progress} % loaded</Html>
 }
 
-function TargetCamera() {
-  const ref = useRef()
-  const { target, lerping } = useStore((state) => state)
-
-  useFrame((_, delta) => {
-    lerping && ref.current.target.lerp(target, delta * 10)
+function Tween() {
+  useFrame(() => {
+    TWEEN.update()
   })
-  return <OrbitControls ref={ref} target={[4, 0, 0]} />
 }
 
 export default function App() {
-  const { setLerping } = useStore((state) => state)
-
+  const ref = useRef()
   return (
     <>
-      <Canvas
-        shadows
-        camera={{ position: [4, 0, 3] }}
-        onPointerDown={() => setLerping(false)}>
+      <Canvas shadows camera={{ position: [4, 0, 3] }}>
         <Suspense fallback={<Loader />}>
           <Environment preset="forest" />
-          <Model />
-          <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
-          <TargetCamera />
+          <OrbitControls ref={ref} target={[4, 0, 0]} />
+          <Model controls={ref} />
+          <Tween />
         </Suspense>
       </Canvas>
       <div id="instructions">Doubleclick to change OrbitControls target</div>
--- src/Scan.jsx
+++ src/Scan.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
-import { useStore } from './App'
+import TWEEN from '@tweenjs/tween.js'
 
-export function Model(props) {
+export function Model({ controls }) {
   const { nodes, materials } = useGLTF('./models/scan-transformed.glb')
-  const { target, setLerping } = useStore((state) => state)
 
   return (
-    <group {...props} dispose={null}>
+    <group dispose={null}>
       <mesh geometry={nodes.Mesh_0.geometry} material={materials.material_0} />
 
       {/* A box used for raycasting since the photogrammetry geometry has so many faces that it makes the raycaster slow */}
@@ -15,8 +14,17 @@ export function Model(props) {
         position={[0, -0.25, -0.6]}
         rotation-y={-Math.PI / 64}
         onDoubleClick={({ point }) => {
-          target.copy(point)
-          setLerping(true)
+          new TWEEN.Tween(controls.current.target)
+            .to(
+              {
+                x: point.x,
+                y: point.y,
+                z: point.z
+              },
+              500
+            )
+            .easing(TWEEN.Easing.Cubic.Out)
+            .start()
         }}>
         <boxGeometry args={[30, 3, 0.1]} />
         <meshBasicMaterial wireframe visible={false} />
