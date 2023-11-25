8f1c7c7d676d814b37337f3c9167ec0cc7944b4c
Sean Bradley <seanwasere@gmail.com>
2023-11-25 23:21:35 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { Canvas } from '@react-three/fiber'
-import { Stats, OrbitControls } from '@react-three/drei'
+import { Stats, OrbitControls, Html, useProgress } from '@react-three/drei'
 import Eve from './Eve'
+import { Suspense } from 'react'
+
+function Loader() {
+  return <div className="loader"></div>
+}
 
 export default function App() {
   return (
@@ -20,13 +25,15 @@ export default function App() {
           Mixamo
         </a>
       </div>
-      <Canvas camera={{ position: [0, 1, 1] }} shadows>
-        <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
-        <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
-        <Eve />
-        <OrbitControls target={[0, 0.75, 0]} />
-        <Stats />
-      </Canvas>
+      <Suspense fallback={<Loader />}>
+        <Canvas camera={{ position: [0, 1, 1] }} shadows>
+          <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
+          <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
+          <Eve />
+          <OrbitControls target={[0, 0.75, 0]} />
+          <Stats />
+        </Canvas>
+      </Suspense>
     </>
   )
 }
