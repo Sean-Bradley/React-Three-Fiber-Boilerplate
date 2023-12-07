c39caa993e96ba502c3d6ce79c85d3db9d9c0d67
Sean Bradley <seanwasere@gmail.com>
2023-12-07 22:10:24 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,59 @@
-import { Stats, OrbitControls, Environment } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { useRef } from 'react'
-import { Debug, Physics, useBox, usePlane } from '@react-three/cannon'
-import { useControls } from 'leva'
+import { Stats, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import { Debug, Physics, useBox, usePlane } from "@react-three/cannon";
+import { useControls } from "leva";
 
 function Plane(props) {
-  usePlane(() => ({ ...props }))
+  usePlane(() => ({ ...props }));
 }
 
-function Box(props) {
-  const [ref, api] = useBox(
-    () => ({ args: [1, 1, 1], mass: 0, type: 'Dynamic', ...props }),
-    useRef()
-  )
+function InstancedBoxes() {
+  const [ref, { at }] = useBox(
+    (i) => ({
+      args: [1, 1, 1],
+      type: "Dynamic",
+      position: [
+        Math.floor(i % 8) * 1.01 - 4,
+        Math.floor((i / 64) % 64) * 1.01 + 4,
+        Math.floor((i / 8) % 8) * 1.01 - 4,
+      ],
+    }),
+    useRef(),
+  );
 
   return (
-    <mesh
+    <instancedMesh
       ref={ref}
+      args={[undefined, undefined, 512]}
       onPointerDown={(e) => {
-        e.stopPropagation()
-        api.mass.set(1)
-      }}>
+        e.stopPropagation();
+        at(e.instanceId).mass.set(1);
+      }}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshNormalMaterial />
-    </mesh>
-  )
+    </instancedMesh>
+  );
 }
 
 export default function App() {
-  const gravity = useControls('Gravity', {
+  const gravity = useControls("Gravity", {
     x: { value: 0, min: -10, max: 10, step: 0.1 },
     y: { value: -9.8, min: -10, max: 10, step: 0.1 },
-    z: { value: 0, min: -10, max: 10, step: 0.1 }
-  })
+    z: { value: 0, min: -10, max: 10, step: 0.1 },
+  });
+
   return (
-    <Canvas camera={{ position: [0, 4, 4] }}>
-      <Environment
-        preset="forest"
-        background
-        ground={{
-          height: 2,
-          radius: 115,
-          scale: 100
-        }}
-      />
+    <Canvas camera={{ position: [6, 9, 9] }}>
       <Physics gravity={[gravity.x, gravity.y, gravity.z]}>
-        <Debug>
+        <Debug color={0x004400}>
           <Plane rotation={[-Math.PI / 2, 0, 0]} />
-          {[...Array(6).keys()].map((x) =>
-            [...Array(6).keys()].map((z) => (
-              <Box
-                key={x + z * 6}
-                position={[(x - 2.5) * 1.01, 2, (z - 2.5) * 1.01]}
-              />
-            ))
-          )}
         </Debug>
+        <InstancedBoxes />
       </Physics>
-      <OrbitControls target-y={0.5} enableZoom={false} />
+      <OrbitControls target-y={5} />
       <Stats />
     </Canvas>
-  )
+  );
 }
