86d0f6087931d332916f9a4366942aaa89918dda
Sean Bradley <seanwasere@gmail.com>
2023-12-08 09:20:52 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,86 @@
-import { Canvas } from '@react-three/fiber'
-import { Stats, OrbitControls } from '@react-three/drei'
-import Eve from './Eve'
-import { Suspense } from 'react'
+import { useRef, forwardRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Stats, OrbitControls, Environment, useGLTF } from '@react-three/drei'
+import { MeshStandardMaterial } from 'three'
+import TWEEN from '@tweenjs/tween.js'
 
-function Loader() {
-  return <div className="loader"></div>
+function Plane({ monkey }) {
+  return (
+    <mesh
+      position-y={-1}
+      rotation-x={-Math.PI / 2}
+      receiveShadow
+      onDoubleClick={({ point }) => {
+        new TWEEN.Tween(monkey.current.position)
+          .to(
+            {
+              x: point.x,
+              z: point.z
+            },
+            500
+          )
+          .start()
+
+        new TWEEN.Tween(monkey.current.position)
+          .to(
+            {
+              y: point.y + 3
+            },
+            250
+          )
+          .easing(TWEEN.Easing.Cubic.Out)
+          .start()
+          .onComplete(() => {
+            new TWEEN.Tween(monkey.current.position)
+              .to(
+                {
+                  y: point.y + 1
+                },
+                250
+              )
+              .easing(TWEEN.Easing.Bounce.Out)
+              .start()
+          })
+      }}>
+      <planeGeometry args={[10, 10]} />
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
+
+const Monkey = forwardRef(function Monkey({ position }, ref) {
+  const { scene } = useGLTF('/models/suzanne.glb')
+  scene.children.forEach((m) => {
+    m.castShadow = true
+    m.material = new MeshStandardMaterial({ flatShading: true })
+  })
+  return (
+    <group ref={ref} position={position} castShadow>
+      <primitive object={scene} />
+    </group>
+  )
+})
+
+function Tween() {
+  useFrame(() => {
+    TWEEN.update()
+  })
 }
 
 export default function App() {
+  const ref = useRef()
   return (
     <>
-       <div id="instructions">
-        <kbd>W</kbd> to walk
-        <br />
-        <kbd>W</kbd> & <kbd>⇧ Shift</kbd> to run.
-        <br />
-        <kbd>Space</kbd> to jump
-        <br />
-        <kbd>Q</kbd> to fancy pose
-        <br />
-        <br />
-        Model from{" "}
-        <a
-          href="https://www.mixamo.com"
-          target="_blank"
-          rel="nofollow noreferrer"
-        >
-          Mixamo
-        </a>
-      </div>
-      <Suspense fallback={<Loader />}>
-        <Canvas camera={{ position: [0, 1, 1] }} shadows>
-          <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
-          <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
-          <Eve />
-          <OrbitControls target={[0, 0.75, 0]} />
-          <Stats />
-        </Canvas>
-      </Suspense>
+      <Canvas camera={{ position: [0, 0, 3] }} shadows>
+        <Environment preset="forest" />
+        <directionalLight position={[1, 1, 1]} intensity={Math.PI} castShadow />
+        <OrbitControls />
+        <Monkey ref={ref} />
+        <Plane monkey={ref} />
+        <Tween />
+        <Stats />
+      </Canvas>
+      <div id="instructions">Doubleclick anywhere on the plane to bounce the monkey to it</div>
     </>
   )
 }
