46c1e0d9c46abf8039163d17bbde52a8099109cb
Sean Bradley <seanwasere@gmail.com>
2023-12-08 23:46:14 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, './models/suzanne.glb')
+  const gltf = useLoader(GLTFLoader, '/models/suzanne.glb')
 
   return (
     <>
