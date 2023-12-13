430f2eec3e0d742bcba30611cb62cb01eb45fca8
Sean Bradley <seanwasere@gmail.com>
2023-12-13 15:02:23 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { Stats, OrbitControls, Circle } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, '/models/suzanne.glb')
+  const gltf = useLoader(GLTFLoader, '/models/monkey.glb')
 
   return (
-    <>
-      <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-        <directionalLight position={[3.3, 1.0, 4.4]} castShadow={true} intensity={Math.PI} />
-        <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
-        <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true}>
-          <meshStandardMaterial />
-        </Circle>
-        <OrbitControls target={[0, 1, 0]} />
-        <axesHelper args={[5]} />
-        <Stats />
-      </Canvas>
-    </>
+    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <directionalLight position={[3.3, 1.0, 4.4]} castShadow />
+      <primitive
+        object={gltf.scene}
+        position={[0, 1, 0]}
+        children-0-castShadow
+      />
+      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+        <meshStandardMaterial />
+      </Circle>
+      <OrbitControls target={[0, 1, 0]} />
+      <axesHelper args={[5]} />
+      <Stats />
+    </Canvas>
   )
 }
