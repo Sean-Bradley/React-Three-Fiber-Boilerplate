b262ac01ce1f5be3efe94da5d58382c9b7d5b8cc
sean bradley <seanwasere@gmail.com>
2024-06-18 22:47:33 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
 
   return (
     <Canvas camera={{ position: [-0.5, 1, 2] }}>
-      <Environment files="./img/venice_sunset_1k.hdr" background blur={0.5} />
+      <Environment files="./img/venice_sunset_1k.hdr" background backgroundBlurriness={1} />
       <directionalLight position={[3.3, 1.0, 4.4]} />
       <primitive object={gltf.scene} position={[0, 1, 0]} />
       <OrbitControls target={[0, 1, 0]} autoRotate />
