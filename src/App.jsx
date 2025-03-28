4756e054beb5417ed06a7e8d49cbe804f9e7edac
sean bradley <seanwasere@gmail.com>
2025-03-28 09:54:03 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import { Model } from './Shoe'
+import { Leva } from 'leva'
 
 export default function App() {
-  console.log("in app")
+  console.log('in app')
   return (
-    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
-      <Environment preset="forest" />
-      <Model />
-      <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
-      <OrbitControls autoRotate />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+        <Environment preset="forest" />
+        <Model />
+        <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
+        <OrbitControls autoRotate />
+      </Canvas>
+      <Leva />
+    </>
   )
 }
