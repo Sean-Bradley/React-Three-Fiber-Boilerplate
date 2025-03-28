332ee04d633dfe10845c48cee690bc3c8a9a82aa
sean bradley <seanwasere@gmail.com>
2025-03-28 09:37:54 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
 import { Stats, OrbitControls } from '@react-three/drei'
+import { Leva } from 'leva'
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [-1, 4, 2.5] }}>
-      <directionalLight position={[1, 1, 1]} />
-      <Polyhedron name="meshBasicMaterial" position={[-3, 1, 0]} material={new THREE.MeshBasicMaterial()} />
-      <Polyhedron name="meshNormalMaterial" position={[-1, 1, 0]} material={new THREE.MeshNormalMaterial()} />
-      <Polyhedron name="meshPhongMaterial" position={[1, 1, 0]} material={new THREE.MeshPhongMaterial()} />
-      <Polyhedron name="meshStandardMaterial" position={[3, 1, 0]} material={new THREE.MeshStandardMaterial()} />
-      <OrbitControls target-y={1} />
-      <axesHelper args={[5]} />
-      <gridHelper />
-      <Stats />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [-1, 4, 2.5] }}>
+        <directionalLight position={[1, 1, 1]} />
+        <Polyhedron name="meshBasicMaterial" position={[-3, 1, 0]} material={new THREE.MeshBasicMaterial()} />
+        <Polyhedron name="meshNormalMaterial" position={[-1, 1, 0]} material={new THREE.MeshNormalMaterial()} />
+        <Polyhedron name="meshPhongMaterial" position={[1, 1, 0]} material={new THREE.MeshPhongMaterial()} />
+        <Polyhedron name="meshStandardMaterial" position={[3, 1, 0]} material={new THREE.MeshStandardMaterial()} />
+        <OrbitControls target-y={1} />
+        <axesHelper args={[5]} />
+        <gridHelper />
+        <Stats />
+      </Canvas>
+      <Leva />
+    </>
   )
 }
