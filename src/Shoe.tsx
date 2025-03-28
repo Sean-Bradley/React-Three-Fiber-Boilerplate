f9e1df46c9ab16cc05e79a4cfdce33dcfb2ff1c5
sean bradley <seanwasere@gmail.com>
2025-03-28 17:06:39 +0000

    updates

--- src/Shoe.tsx
+++ src/Shoe.tsx
@@ -2,19 +2,12 @@ import { useEffect, useState } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { useControls } from 'leva'
 import { Color } from 'three'
-import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
-
-interface IGLTF extends GLTF {
-  nodes: { [key: string]: THREE.Mesh }
-  materials: { [key: string]: THREE.MeshStandardMaterial }
-}
+import * as THREE from 'three'
 
 export function Model() {
   const [hovered, setHovered] = useState(false)
 
-  const { nodes, materials } = useGLTF(
-    './models/shoe-draco.glb'
-  ) as IGLTF
+  const { nodes, materials } = useGLTF('./models/shoe-draco.glb')
 
   useEffect(() => {
     document.body.style.cursor = hovered ? 'pointer' : 'auto'
@@ -32,7 +25,9 @@ export function Model() {
               '#' +
               ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0'),
             onChange: (v: THREE.Color) => {
-              materials[m].color = new Color(v)
+              ;(materials[m] as THREE.MeshStandardMaterial).color = new Color(
+                v
+              )
             }
           }
         }),
@@ -50,22 +45,47 @@ export function Model() {
       onPointerOut={() => setHovered(false)}
       onClick={(e) => {
         e.stopPropagation()
-        ;(document.getElementById(
-          'Shoe.' + ((e.object as THREE.Mesh).material as THREE.Material).name
-        ) as HTMLElement).focus()
+        ;(
+          document.getElementById(
+            'Shoe.' +
+              ((e.object as THREE.Mesh).material as THREE.Material).name
+          ) as HTMLElement
+        ).focus()
       }}>
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+      <mesh
+        geometry={(nodes.shoe as THREE.Mesh).geometry}
+        material={materials.laces}
+      />
+      <mesh
+        geometry={(nodes.shoe_1 as THREE.Mesh).geometry}
+        material={materials.mesh}
+      />
+      <mesh
+        geometry={(nodes.shoe_2 as THREE.Mesh).geometry}
+        material={materials.caps}
+      />
+      <mesh
+        geometry={(nodes.shoe_3 as THREE.Mesh).geometry}
+        material={materials.inner}
+      />
+      <mesh
+        geometry={(nodes.shoe_4 as THREE.Mesh).geometry}
+        material={materials.sole}
+      />
+      <mesh
+        geometry={(nodes.shoe_5 as THREE.Mesh).geometry}
+        material={materials.stripes}
+      />
+      <mesh
+        geometry={(nodes.shoe_6 as THREE.Mesh).geometry}
+        material={materials.band}
+      />
+      <mesh
+        geometry={(nodes.shoe_7 as THREE.Mesh).geometry}
+        material={materials.patch}
+      />
     </group>
   )
 }
 
-useGLTF.preload(
-  './models/shoe-draco.glb'
-)
+useGLTF.preload('./models/shoe-draco.glb')
