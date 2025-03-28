53d9f4221348ec7aa9011b76f1add259d680ffb8
sean bradley <seanwasere@gmail.com>
2025-03-28 09:40:25 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
 import { Stats, OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
+import { Leva, useControls } from 'leva'
 import { useRef } from 'react'
 
 function Lights() {
@@ -107,32 +107,35 @@ function Lights() {
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [4, 4, 1.5] }}>
-      <Lights />
-      <Polyhedron
-        name="meshBasicMaterial"
-        position={[-3, 1, 0]}
-        material={new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })}
-      />
-      <Polyhedron
-        name="meshNormalMaterial"
-        position={[-1, 1, 0]}
-        material={new THREE.MeshNormalMaterial({ flatShading: true })}
-      />
-      <Polyhedron
-        name="meshPhongMaterial"
-        position={[1, 1, 0]}
-        material={new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })}
-      />
-      <Polyhedron
-        name="meshStandardMaterial"
-        position={[3, 1, 0]}
-        material={new THREE.MeshStandardMaterial({ color: 0xff0033, flatShading: true })}
-      />
-      <OrbitControls target={[2, 2, 0]} />
-      <axesHelper args={[5]} />
-      <gridHelper />
-      <Stats />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [4, 4, 1.5] }}>
+        <Lights />
+        <Polyhedron
+          name="meshBasicMaterial"
+          position={[-3, 1, 0]}
+          material={new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })}
+        />
+        <Polyhedron
+          name="meshNormalMaterial"
+          position={[-1, 1, 0]}
+          material={new THREE.MeshNormalMaterial({ flatShading: true })}
+        />
+        <Polyhedron
+          name="meshPhongMaterial"
+          position={[1, 1, 0]}
+          material={new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })}
+        />
+        <Polyhedron
+          name="meshStandardMaterial"
+          position={[3, 1, 0]}
+          material={new THREE.MeshStandardMaterial({ color: 0xff0033, flatShading: true })}
+        />
+        <OrbitControls target={[2, 2, 0]} />
+        <axesHelper args={[5]} />
+        <gridHelper />
+        <Stats />
+      </Canvas>
+      <Leva />
+    </>
   )
 }
