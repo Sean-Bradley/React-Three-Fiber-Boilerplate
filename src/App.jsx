88f590be52117d28562439725662734165f903a0
sean bradley <seanwasere@gmail.com>
2025-03-28 09:29:24 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
 import { useMemo } from 'react'
 import { Stats, OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
+import { Leva, useControls } from 'leva'
 
 export default function App() {
   const polyhedron = useMemo(
-    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398), new THREE.DodecahedronGeometry(0.785398)],
+    () => [
+      new THREE.BoxGeometry(),
+      new THREE.SphereGeometry(0.785398),
+      new THREE.DodecahedronGeometry(0.785398)
+    ],
     []
   )
 
@@ -25,25 +29,28 @@ export default function App() {
   const pB = useControls('Polyhedron B', options)
 
   return (
-    <Canvas camera={{ position: [1, 2, 3] }}>
-      <Polyhedron
-        position={[-1, 1, 0]}
-        rotation={[pA.x, pA.y, pA.z]}
-        visible={pA.visible}
-        color={pA.color}
-        polyhedron={polyhedron}
-      />
-      <Polyhedron
-        position={[1, 1, 0]}
-        rotation={[pB.x, pB.y, pB.z]}
-        visible={pB.visible}
-        color={pB.color}
-        polyhedron={polyhedron}
-      />
-      <OrbitControls target-y={1} />
-      <axesHelper args={[5]} />
-      <gridHelper />
-      <Stats />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [1, 2, 3] }}>
+        <Polyhedron
+          position={[-1, 1, 0]}
+          rotation={[pA.x, pA.y, pA.z]}
+          visible={pA.visible}
+          color={pA.color}
+          polyhedron={polyhedron}
+        />
+        <Polyhedron
+          position={[1, 1, 0]}
+          rotation={[pB.x, pB.y, pB.z]}
+          visible={pB.visible}
+          color={pB.color}
+          polyhedron={polyhedron}
+        />
+        <OrbitControls target-y={1} />
+        <axesHelper args={[5]} />
+        <gridHelper />
+        <Stats />
+      </Canvas>
+      <Leva />
+    </>
   )
 }
