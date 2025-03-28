64b15fdde6565d74ca4837ea3d5d80c8dea96d59
sean bradley <seanwasere@gmail.com>
2025-03-28 11:04:53 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { Canvas } from '@react-three/fiber'
 import { Stats, OrbitControls } from '@react-three/drei'
 import Eve from './Eve'
-import { Suspense } from 'react'
-
-function Loader() {
-  return <div className="loader"></div>
-}
 
 export default function App() {
   return (
     <>
-       <div id="instructions">
+      <div id="instructions">
         <kbd>W</kbd> to walk
         <br />
         <kbd>W</kbd> & <kbd>⇧ Shift</kbd> to run.
@@ -20,24 +15,18 @@ export default function App() {
         <kbd>Q</kbd> to fancy pose
         <br />
         <br />
-        Model from{" "}
-        <a
-          href="https://www.mixamo.com"
-          target="_blank"
-          rel="nofollow noreferrer"
-        >
+        Model from{' '}
+        <a href="https://www.mixamo.com" target="_blank" rel="nofollow noreferrer">
           Mixamo
         </a>
       </div>
-      <Suspense fallback={<Loader />}>
-        <Canvas camera={{ position: [0, 1, 1] }} shadows>
-          <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
-          <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
-          <Eve />
-          <OrbitControls target={[0, 0.75, 0]} />
-          <Stats />
-        </Canvas>
-      </Suspense>
+      <Canvas camera={{ position: [0, 1, 1] }} shadows>
+        <spotLight position={[2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
+        <spotLight position={[-2.5, 5, 5]} angle={Math.PI / 3} penumbra={0.5} castShadow shadow-mapSize-height={2048} shadow-mapSize-width={2048} intensity={Math.PI * 50} />
+        <Eve />
+        <OrbitControls target={[0, 0.75, 0]} />
+        <Stats />
+      </Canvas>
     </>
   )
 }
